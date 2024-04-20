c9ae32c761a57a777a247aa416aca4fdb73f48af
fad16papa <fad16papa@gmail.com>
2024-04-20 22:12:16 +0800

    Add auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { UserButton } from "@clerk/nextjs";
 
 const Home = () => {
-  return <div>Home</div>;
+  return <UserButton afterSignOutUrl="/" />;
 };
 
 export default Home;
