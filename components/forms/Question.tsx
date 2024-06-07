f0a760a8b74cceb0003cdf77efe24b097831e794
fad16papa <fad16papa@gmail.com>
2024-06-08 02:05:26 +0800

    Commit changes

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -21,12 +21,19 @@ import { QuestionsSchema } from "@/lib/validations";
 import { Badge } from "../ui/badge";
 import Image from "next/image";
 import { createQuestion } from "@/lib/actions/question.action";
+import { useRouter, usePathname } from "next/navigation";
 
 const type: any = "create";
 
-const Question = () => {
+interface Props {
+  mongoUserId: string;
+}
+
+const Question = ({ mongoUserId }: Props) => {
   const editorRef = useRef(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const router = useRouter();
+  const pathname = usePathname();
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof QuestionsSchema>>({
@@ -44,8 +51,15 @@ const Question = () => {
 
     try {
       // Handle the form submission here.
-      await createQuestion({});
-      console.log("im here");
+      await createQuestion({
+        title: values.title,
+        content: values.explanation,
+        tags: values.tags,
+        author: JSON.parse(mongoUserId),
+      });
+
+      // Navigate to the home page
+      router.push("/");
     } catch (error) {
     } finally {
       setIsSubmitting(false);
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,11 +1,36 @@
 "use server";
 
+import Question from "@/database/question.mode";
 import { connectToDatabase } from "../mongoose";
+import Tag from "@/database/tag.model";
 
 export async function createQuestion(params: any) {
   // Create a new question
   // eslint-disable-next-line no-empty
   try {
     connectToDatabase();
+
+    const { title, content, tags, author } = params;
+
+    // Create a new question
+    const question = await Question.create({ title, content, author });
+
+    const tagDocuments = [];
+
+    // create tags or get them if they already existed
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate(
+        { name: { $regex: new RegExp(`^${tag}$`, "i") } },
+        { $setOnInsert: { name: tag }, $push: { questions: question._id } },
+        { upsert: true, new: true }
+      );
+      tagDocuments.push(existingTag._id);
+    }
+
+    await Question.findByIdAndUpdate(question._id, {
+      $push: { tags: tagDocuments },
+    });
+
+    // create an interaction record for the user's ask_question action
   } catch (error) {}
 }
